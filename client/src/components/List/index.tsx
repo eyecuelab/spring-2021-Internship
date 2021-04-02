fe9b8045162a66b6455ba84c438c7e00510ba27e
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-04-02 15:17:13 -0700

    create-react-app initialize
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

---
+++ client/src/components/List/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Title} from './style';
+
+const List = () => {
+
+  return (
+    <>
+      <Title>This is the list component</Title>
+    </>
+  );
+}
+
+export default List;
