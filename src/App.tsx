9afac1cf277b3b93b2de44a8567bbe5f0e89c30f
Zahnen Garner <zahnen@gmail.com>
2021-04-05 13:02:57 -0700

    add redirect if user is already logged in
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useStore } from 'react-redux';
 import LoginForm from './containers/loginForm';
 import UserHub from './containers/userHub';
 import './App.css';
 
 function App(): JSX.Element {
+  const store = useStore();
+  const loggedIn = store.getState().user.uuid;
+
   return (
     <Router>
       <Switch>
         <Route path="/hub">
           <UserHub />
         </Route>
-        <Route path="/">
-          <LoginForm />
-        </Route>
+        <Route path="/">{loggedIn !== '' ? <Redirect to="/hub" /> : <LoginForm />}</Route>
       </Switch>
     </Router>
   );
