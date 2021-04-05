53b3083944839b5137a1f2a8fcf147713d8f66d8
Zahnen Garner <zahnen@gmail.com>
2021-04-05 11:48:18 -0700

    install and configure redux-persist
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,28 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 /* eslint-disable import/no-cycle */
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../containers/loginForm/loginSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    user: userReducer,
-  },
+const reducers = combineReducers({
+  counter: counterReducer,
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
