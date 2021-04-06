40b9b0a87ca0435a6c8db0f94c6e4b67df58e04c
Zahnen Garner <zahnen@gmail.com>
2021-04-05 17:23:55 -0700

    add modal toggle and functionality to userHub
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/containers/userHub/index.tsx
+++ src/containers/userHub/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import NewProjectModal from '../../components/newProjectModal';
 
 const UserHub = (): JSX.Element => {
+  const [showModal, setModalView] = useState(false);
+  const [projectName, setProjectName] = useState('');
+
+  const handleToggle = () => {
+    setModalView(!showModal);
+  };
+
+  const handleNameUpdate = (name: string) => {
+    setProjectName(name);
+  };
+
   return (
     <>
       <p>This is the user hub, you made it.</p>
+      <button type="button" onClick={handleToggle}>
+        Add New Project
+      </button>
+      {showModal && <NewProjectModal nameUpdate={handleNameUpdate} />}
+      {projectName !== '' ? <p>{projectName}</p> : null}
     </>
   );
 };
