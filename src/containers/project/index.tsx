4b9ba3138909841a46d18a6492aec3ad413369a9
Zahnen Garner <zahnen@gmail.com>
2021-04-07 12:23:52 -0700

    add functionality for adding and clearing tasks
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/containers/project/index.tsx
+++ src/containers/project/index.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/store';
+import { addTask, clearTasks } from '../../store/slices/projectSlice';
 import NewTaskModal from '../../components/newTaskModal';
 import List from '../../components/list';
+import Task from '../../components/task';
 
-const Project = () => {
+const Project = (): JSX.Element => {
+  const dispatch = useDispatch();
   const projectName = useSelector((state: RootState) => state.project.projectName);
   const [showModal, setModalView] = useState(false);
 
@@ -12,13 +15,34 @@ const Project = () => {
     setModalView(!showModal);
   };
 
+  const handleClearingTasks = () => {
+    dispatch(clearTasks());
+  };
+
+  const handleAddingTask = (taskName: string, taskStatus: string) => {
+    dispatch(addTask({ taskName, taskStatus }));
+    setModalView(!showModal);
+  };
+
+  // const array
+  // const todoItems = array.map() =>
+  // <li>{task}</li>
+
+  // forEach task where task status are =
+  // create an array of these Tasks
+
   return (
     <>
       <h1>{projectName}</h1>
-      {showModal && <NewTaskModal />}
-      <List title="To Do" toggleModal={handleToggle} />
+      {showModal && <NewTaskModal addNewTask={handleAddingTask} />}
+      <List title="To Do" toggleModal={handleToggle}>
+        <Task />
+      </List>
       <List title="Doing" toggleModal={handleToggle} />
       <List title="Done" toggleModal={handleToggle} />
+      <button type="submit" onClick={handleClearingTasks}>
+        Clear Tasks
+      </button>
     </>
   );
 };
--- src/store/slices/projectSlice.ts
+++ src/store/slices/projectSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface TaskItem {
+  taskName: string;
+  taskStatus: string;
+}
+
 interface ProjectState {
   projectName: string;
   id: string;
-  tasks: Array<string>;
+  tasks: Array<TaskItem>;
 }
-
-// { taskName: string; taskStatus: string }
 
 const initialState: ProjectState = {
   projectName: '',
@@ -30,12 +33,18 @@ export const projectSlice = createSlice({
     setId: (state) => {
       state.id = idMaker(state.projectName);
     },
-    addTask: (state, action: PayloadAction<string>) => {
-      state.tasks = [...state.tasks, action.payload];
+    addTask: (state, action: PayloadAction<{ taskName: string; taskStatus: string }>) => {
+      state.tasks = [
+        ...state.tasks,
+        { taskName: action.payload.taskName, taskStatus: action.payload.taskStatus },
+      ];
+    },
+    clearTasks: (state) => {
+      state.tasks = [];
     },
   },
 });
 
-export const { setProjectName, setId } = projectSlice.actions;
+export const { setProjectName, setId, addTask, clearTasks } = projectSlice.actions;
 
 export default projectSlice.reducer;
