37e7d6da56aa6b8f00dbdf077cb2968d2f06d0af
Zahnen Garner <zahnen@gmail.com>
2021-04-09 16:27:25 -0700

    add date picker to project form
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/components/newProjectModal/index.tsx
+++ src/components/newProjectModal/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import ReactDatePicker from 'react-datepicker';
+import { useForm, Controller } from 'react-hook-form';
+import 'react-datepicker/dist/react-datepicker.css';
 import { Modal } from '../modal';
 
 type ModalProps = {
-  nameUpdate: (projectName: string) => void;
+  createNewProject: (projectName: string, dueDate: Date) => void;
   toggleModal: () => void;
 };
 
 type Inputs = {
   projectName: string;
+  dueDate: Date;
 };
 
-const NewProjectModal = ({ nameUpdate, toggleModal }: ModalProps): JSX.Element => {
+const NewProjectModal = ({ createNewProject, toggleModal }: ModalProps): JSX.Element => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<Inputs>();
   // eslint-disable-next-line
-  const onSubmit = (data: any) => nameUpdate(data.projectName);
-
+  const onSubmit = (data: any) => createNewProject(data.projectName, data.dueDate);
   return (
     <>
       <Modal width="250px" toggleModal={toggleModal}>
@@ -27,6 +30,14 @@ const NewProjectModal = ({ nameUpdate, toggleModal }: ModalProps): JSX.Element =
           {/* eslint-disable react/jsx-props-no-spreading */}
           <input defaultValue="test" {...register('projectName', { required: true })} />
           {errors.projectName && <p>This field is required</p>}
+          {/* <input type="datetime" placeholder="Due Date" {...register('dueDate')} /> */}
+          <Controller
+            control={control}
+            name="dueDate"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <ReactDatePicker onChange={onChange} onBlur={onBlur} selected={value} />
+            )}
+          />
           <input type="submit" />
         </form>
       </Modal>
