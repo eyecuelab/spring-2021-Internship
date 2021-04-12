79f9fc26448bfedd880e060c7361428ce10568d8
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-04-12 12:05:39 -0700

    update form for conditional rendering
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- src/components/newFinance/index.tsx
+++ src/components/newFinance/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Modal } from '../modal';
 
@@ -8,9 +8,12 @@ type ModalProps = {
 };
 
 type Inputs = {
+  category: string;
   itemName: string;
   itemPrice: string;
   quantity: number;
+  minutes: number;
+  date: Date;
 };
 
 const NewFinance = ({ toggleModal, addNewFinance }: ModalProps): JSX.Element => {
@@ -21,17 +24,58 @@ const NewFinance = ({ toggleModal, addNewFinance }: ModalProps): JSX.Element => 
   } = useForm<Inputs>();
   // eslint-disable-next-line
   const onSubmit = (data: any) => addNewFinance(data.itemName, data.itemPrice, data.quantity);
+  const [formType, setFormType] = useState('material');
+
+  const handleChange = (event: any) => {
+    setFormType(event.target.value);
+  };
+
+  const MaterialForm = () => {
+    return (
+      <>
+        <input defaultValue="test" {...register('itemName', { required: true })} />
+        {errors.itemName && <p>This field is required</p>}
+        <input type="number" placeholder="itemPrice" {...register('itemPrice')} />
+        <input type="number" placeholder="quantity" {...register('quantity')} />
+      </>
+    );
+  };
+
+  const LaborForm = () => {
+    return (
+      <>
+        <input defaultValue="test" {...register('itemName', { required: true })} />
+        {errors.itemName && <p>This field is required</p>}
+        <input type="number" placeholder="minutes" {...register('minutes')} />
+        <input type="number" placeholder="date" {...register('date')} />
+      </>
+    );
+  };
+  const OtherForm = () => {
+    return (
+      <>
+        <input defaultValue="test" {...register('itemName', { required: true })} />
+        {errors.itemName && <p>This field is required</p>}
+        <input type="number" placeholder="itemPrice" {...register('itemPrice')} />
+        <input type="number" placeholder="quantity" {...register('quantity')} />
+      </>
+    );
+  };
 
   return (
     <>
       <Modal width="250px" toggleModal={toggleModal}>
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* eslint-disable react/jsx-props-no-spreading */}
-          <input defaultValue="test" {...register('itemName', { required: true })} />
-          {errors.itemName && <p>This field is required</p>}
-          <input type="number" placeholder="itemPrice" {...register('itemPrice')} />
-          <input type="number" placeholder="quantity" {...register('quantity')} />
+          <select onChange={handleChange}>
+            <option value="material">Material</option>
+            <option value="labor">Labor</option>
+            <option value="other">Other</option>
+          </select>
 
+          {formType === 'material' && <MaterialForm />}
+          {formType === 'labor' && <LaborForm />}
+          {formType === 'other' && <OtherForm />}
           <input type="submit" />
         </form>
       </Modal>
