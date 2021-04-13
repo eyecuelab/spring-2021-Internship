4eb0f69dd923b32274db79927ca8f97bc356115d
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-04-12 17:25:53 -0700

    conditionally render isOpen for toggle view
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- src/components/finance/index.tsx
+++ src/components/finance/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Grid, Cell } from 'styled-css-grid';
 
 const Wrapper = styled.div`
   margin: 10px;
+  max-height: ${(props) => (props.open ? '100%' : '0')};
+  overflow: hidden;
+  padding: ${(props) => (props.open ? '25px 0' : '0')};
+  transition: all 0.3s ease-out;
 `;
 
-const Name = styled.h2`
+const HeadingName = styled.h2`
+  margin: 0 10px 0 10px;
+  padding: 3px;
+  background: #d1cfcf;
+`;
+
+const Name = styled.h3`
   margin: 0 10px 0 10px;
 `;
 
@@ -25,26 +35,35 @@ const Finance = ({
   children,
   totals,
 }: FinanceProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    console.log('clicked');
+  };
+
   return (
-    <Wrapper>
-      <Grid columns={3}>
-        <Cell>
-          <Name>{columnOne}</Name>
-        </Cell>
-        <Cell>
-          <Name>{columnTwo}</Name>
-        </Cell>
-        <Cell>
-          <Name>{columnThree}</Name>
-        </Cell>
-      </Grid>
-      {children}
-      <Grid columns={3}>
-        <Cell left={3}>
-          {columnOne === 'Activity' ? <h2>Total Hours {totals}</h2> : <h2>Total ${totals}.00</h2>}
-        </Cell>
-      </Grid>
-    </Wrapper>
+    <>
+      <HeadingName onClick={handleClick}>{columnOne}</HeadingName>
+      <Wrapper open={isOpen}>
+        <Grid columns={3}>
+          <Cell>
+            <Name>{columnOne}</Name>
+          </Cell>
+          <Cell>
+            <Name>{columnTwo}</Name>
+          </Cell>
+          <Cell>
+            <Name>{columnThree}</Name>
+          </Cell>
+        </Grid>
+        {children}
+        <Grid columns={3}>
+          <Cell left={3}>
+            {columnOne === 'Activity' ? <h2>Total Hours {totals}</h2> : <h2>Total ${totals}.00</h2>}
+          </Cell>
+        </Grid>
+      </Wrapper>
+    </>
   );
 };
 
@@ -53,3 +72,19 @@ export default Finance;
 Finance.defaultProps = {
   children: null,
 };
+
+// const Wrapper = styled.div`
+//   margin: 10px;
+//   max-height: ${isOpen ? '100%' : '0'};
+//   overflow: hidden;
+//   padding: ${isOpen ? '25px 0' : '0'};
+//   transition: all 0.3s ease-out;
+// `;
+
+// const HeadingName = styled.h2`
+//   margin: 0 10px 0 10px;
+// `;
+
+// const Name = styled.h3`
+//   margin: 0 10px 0 10px;
+// `;
