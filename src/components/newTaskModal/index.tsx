7b3c09688c99bc837998ca202498fcc59822a619
Zahnen Garner <zahnen@gmail.com>
2021-04-19 17:36:10 -0700

    update new task form with semantic UI
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/components/newTaskModal/index.tsx
+++ src/components/newTaskModal/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, Button, Select } from 'semantic-ui-react';
 import { useForm } from 'react-hook-form';
 import { Modal } from '../modal';
 
@@ -20,21 +21,32 @@ const NewTaskModal = ({ addNewTask, toggleModal }: ModalProps): JSX.Element => {
   } = useForm<Inputs>();
   // eslint-disable-next-line
   const onSubmit = (data: any) => addNewTask(data.taskName, data.taskStatus);
-
+  const statusOptions = [
+    { key: 'todo', value: 'todo', text: 'To Do' },
+    { key: 'doing', value: 'doing', text: 'Doing' },
+    { key: 'done', value: 'done', text: 'Done' },
+  ];
   return (
     <>
       <Modal toggleModal={toggleModal} width="350px">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input defaultValue="test" {...register('taskName', { required: true })} />
-          {errors.taskName && <p>This field is required</p>}
-
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Field>
+            <input placeholder="Task Name" {...register('taskName', { required: true })} />
+            {errors.taskName && <p>This field is required</p>}
+          </Form.Field>
           <select {...register('taskStatus', { required: true })}>
             <option value="todo">To Do</option>
             <option value="doing"> Doing</option>
             <option value="done"> Done</option>
           </select>
-          <input type="submit" />
-        </form>
+          {/* <Select
+            placeholder="Status"
+            options={statusOptions}
+            {...register('taskStatus', { required: true })}
+          /> */}
+
+          <Button type="submit">Submit</Button>
+        </Form>
       </Modal>
     </>
   );
