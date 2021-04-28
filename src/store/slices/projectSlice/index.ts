faa7609da3c07ff463cd417578453e48f82254fc
Zahnen Garner <zahnen@gmail.com>
2021-04-28 15:09:34 -0700

    add post/taskactivity to updateTask thunk

--- src/store/slices/projectSlice/index.ts
+++ src/store/slices/projectSlice/index.ts
@@ -48,21 +48,31 @@ export const updateTask = createAsyncThunk(
   'tasks/updateTask',
   async (
     {
+      taskName,
       intId,
       taskStatus,
       updatedPosition,
-    }: { intId: number; taskStatus: string; updatedPosition: number },
+    }: { taskName: string; intId: number; taskStatus: string; updatedPosition: number },
     thunkAPI
   ) => {
     try {
-      const response = await axios.put(`http://localhost:3000/api/tasks/${intId}`, {
+      const now = dayjs().toString();
+      const taskResponse = await axios.put(`http://localhost:3000/api/tasks/${intId}`, {
         task: {
           id: intId,
           taskStatus,
           position: updatedPosition,
         },
       });
-      return response.data;
+      await axios.post(`http://localhost:3000/api/task-activities`, {
+        taskActivity: {
+          dateTime: now,
+          description: `${taskName} was moved to ${taskStatus}`,
+          task: intId,
+        },
+      });
+
+      return taskResponse.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
     }
