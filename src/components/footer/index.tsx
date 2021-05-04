30f6555585cd218229a2f335359dafba2cf58466
Zahnen Garner <zahnen@gmail.com>
2021-05-04 16:49:05 -0700

    add container in footer wrapper

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  width: 1120px;
   height: 51px;
   left: 0px;
   top: 2424px;
   background: ${(props) => props.theme.colors.cloudyGreen};
   border-top: 2px ${(props) => props.theme.colors.white} dashed;
   text-align: center;
+  padding: 0.25rem 9999rem;
+  margin-right: -9999rem;
+  margin-left: -9999rem;
+`;
+
+const Container = styled.div`
+  width: 1120px;
 `;
 
 const Footer = (): JSX.Element => {
   return (
     <>
       <Wrapper>
-        <h1 style={{ color: 'white' }}>CURO</h1>
+        <Container>
+          <h1 style={{ color: 'white' }}>CURO</h1>
+        </Container>
       </Wrapper>
     </>
   );
