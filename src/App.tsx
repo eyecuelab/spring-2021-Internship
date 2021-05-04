91f5bae6ed29680026e99b72f5b0c7e52386e96c
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-05-04 11:46:20 -0700

    add global styles and theme
    Co-authored-by: Zahnen <Zahnen@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import * as selectors from './store/selectors';
 import LoginForm from './containers/loginForm';
 import UserHub from './containers/userHub';
 import Project from './containers/project';
 import NavBar from './components/navBar';
+import theme from './styles/theme';
 import './App.css';
+import GlobalStyles from './styles/globalStyles';
 
 function App(): JSX.Element {
   const loggedIn = useSelector(selectors.selectUUID);
 
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/project">{!loggedIn ? <Redirect to="/" /> : <Project />}</Route>
-        <Route path="/hub">{!loggedIn ? <Redirect to="/" /> : <UserHub />}</Route>
-        <Route path="/">{loggedIn ? <Redirect to="/hub" /> : <LoginForm />}</Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/project">{!loggedIn ? <Redirect to="/" /> : <Project />}</Route>
+          <Route path="/hub">{!loggedIn ? <Redirect to="/" /> : <UserHub />}</Route>
+          <Route path="/">{loggedIn ? <Redirect to="/hub" /> : <LoginForm />}</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+body { 
+  max-width: 1120px;
+  margin: auto;
+}
+`;
+
+export default GlobalStyles;
