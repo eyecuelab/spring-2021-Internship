5046411f40fe7a15bbf1260aac504013c4bc10c7
Zahnen Garner <zahnen@gmail.com>
2021-05-05 15:13:25 -0700

    add smallButton component

---
+++ src/components/smallButton/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div<{ margin: string }>`
+  position: absolute;
+  margin: ${(props) => props.margin};
+`;
+
+const ButtonImg = styled.img`
+  position: absolute;
+`;
+
+const ButtonStyle = styled.button<{
+  size: string;
+  color: string;
+}>`
+  margin-top: 3px;
+  width: 140px;
+  position: absolute;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: ${(props) => props.size};
+  z-index: 20;
+  padding: 20px;
+  font-family: ${(props) => props.theme.font};
+  color: ${(props) => props.color};
+`;
+
+type ButtonProps = {
+  buttonText: string;
+  size: string;
+  margin: string;
+  img: string;
+  color: string;
+};
+
+const SmallButton = ({ buttonText, size, margin, img, color }: ButtonProps): JSX.Element => {
+  return (
+    <ButtonWrapper margin={margin}>
+      <ButtonStyle size={size} color={color}>
+        {buttonText}
+      </ButtonStyle>
+      <ButtonImg src={img} />
+    </ButtonWrapper>
+  );
+};
+
+export default SmallButton;
