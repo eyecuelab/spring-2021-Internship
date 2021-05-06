f1734e92e382672b74e31ab86e49953807db5ffe
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-05-06 16:53:02 -0700

    update styling and functionality of taskDetail Modal

--- src/components/taskDetail/index.tsx
+++ src/components/taskDetail/index.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
 import { Modal } from '../modal';
 import { TaskItem } from '../../store/slices/projectSlice';
 import theme from '../../styles/theme';
+// import { Display, Edit } from '../../containers/project/components';
+// import InlineEdit from '../inlineEdit';
+import { updateTask } from '../../store/slices/projectSlice/thunks';
 
-const LogWrapper = styled.div`
-  margin: auto;
-  width: 325px;
-  max-height: 200px;
-  overflow: scroll;
+const Header = styled.h1`
+  font-family: ${(props) => props.theme.font};
+  font-size: ${(props) => props.theme.fontSizes.large};
+  color: ${(props) => props.theme.colors.white};
+  text-alight: left;
+  margin-bottom: 36px;
+`;
+
+const Input = styled.input`
+  width: 584px;
+  height: 54px;
+  font-family: ${(props) => props.theme.font};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  padding: 18px;
+  margin-bottom: 24px;
+`;
+
+const TextArea = styled.textarea`
+  width: 584px;
+  min-height: 151px;
+  font-family: ${(props) => props.theme.font};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  padding: 18px;
+`;
+
+const ButtonWrapper = styled.div`
   display: flex;
-  flex-direction: column-reverse;
-  overflow-x: hidden;
+`;
+
+const Button = styled.button<{ margin: string }>`
+  display: inline;
+  margin-top: 22px;
+  margin-left: ${(props) => props.margin};
+  background: none;
+  border: none;
+  cursor: pointer;
+  text-decoration: none;
+  font-family: ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.white};
 `;
 
 type TaskDetailProps = {
@@ -21,35 +57,85 @@ type TaskDetailProps = {
   deleteTask: (id: string, taskStatus: string) => void;
 };
 
+type Inputs = {
+  taskName: string;
+  taskStatus: string;
+  taskDesc: string;
+};
+
 const TaskDetail = ({ toggleModal, task, deleteTask }: TaskDetailProps): JSX.Element => {
-  const ActivityItems: JSX.Element[] = task.activity.map((e) => {
-    const date = dayjs(e.dateTime).format('ddd MM/DD/YYYY h:mm a');
-    return (
-      <>
-        <div style={{ background: 'grey', margin: '5px' }}>
-          <p>{e.description}</p>
-          <p>{date}</p>
-        </div>
-      </>
-    );
-  });
+  const dispatch = useDispatch();
+
+  const handleUpdateTask = (
+    taskName: string,
+    taskId: string,
+    taskStatus: string,
+    taskDesc: string,
+    updatedPosition: number
+  ) => {
+    const intId = parseInt(taskId, 10);
+    console.log({ taskName });
+    dispatch(updateTask({ taskName, intId, taskStatus, taskDesc, updatedPosition }));
+  };
+
+  // const handleEditTaskName = (updatedValue: string | number) => {
+  //   console.log(updatedValue.toString());
+  //   handleUpdateTask(
+  //     task?.id,
+  //     updatedValue.toString(),
+  //     task?.taskStatus,
+  //     task?.taskDesc,
+  //     task?.position
+  //   );
+  // };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit = (data: any) => {
+    console.log('submit?');
+    handleUpdateTask(data.taskName, task.id, task.taskStatus, data.taskDesc, task.position);
+    toggleModal();
+  };
   return (
     <>
-      <Modal width="664px" toggleModal={toggleModal} color={theme.colors.burntOrange}>
+      <Modal width="664px" color={theme.colors.burntOrange}>
         <>
-          <h2>Task Detail</h2>
-          <h3>{task?.taskName}</h3>
-          <h4>Status: {task?.taskStatus}</h4>
-          <h4>{task.taskDesc}</h4>
-          <h4>Activity:</h4>
-          <button type="button" onClick={() => deleteTask(task?.id, task?.taskStatus)}>
-            Delete Task
-          </button>
-          <LogWrapper>{ActivityItems}</LogWrapper>
+          <Header>Task Detail</Header>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input defaultValue={task.taskName} {...register('taskName', { required: true })} />
+            {errors.taskName && <p>This field is required</p>}
+            <TextArea defaultValue={task.taskDesc} {...register('taskDesc')} />
+            <button type="button" onClick={() => deleteTask(task?.id, task?.taskStatus)}>
+              delete
+            </button>
+            <ButtonWrapper>
+              <Button type="button" onClick={toggleModal} margin="458px">
+                Cancel
+              </Button>
+              <Button type="submit" margin="15px">
+                Save
+              </Button>
+            </ButtonWrapper>
+          </form>
         </>
       </Modal>
     </>
   );
+  // return (
+  //   <>
+  //     <Modal width="664px" toggleModal={toggleModal} color={theme.colors.burntOrange}>
+  //       <>
+  //         <h2>Task Detail</h2>
+  //         <h3>{task.taskName}</h3>
+  //         <h4>Status: {task.taskStatus}</h4>
+  //         <h4>{task.taskDesc}</h4>
+  //
+  //       </>
+  //     </Modal>
+  //   </>
+  // );
 };
 
 export default TaskDetail;
