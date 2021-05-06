34a008ee7cc13e463a27143e75a011cc86e86f11
Zahnen Garner <zahnen@gmail.com>
2021-05-05 18:04:06 -0700

    reposition login component

--- src/containers/loginForm/index.tsx
+++ src/containers/loginForm/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { GoogleLogin } from 'react-google-login';
+import styled from 'styled-components';
 import { signIn } from '../../store/slices/userSlice/thunks';
 
 const clientId: string = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID ?? '';
+const Wrapper = styled.div`
+  margin-top: 200px;
+`;
 
 const LoginForm = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -14,13 +18,15 @@ const LoginForm = (): JSX.Element => {
 
   return (
     <>
-      <GoogleLogin
-        clientId={clientId}
-        buttonText="Log in with Google"
-        onSuccess={handleLogin}
-        onFailure={handleLogin}
-        cookiePolicy="single_host_origin"
-      />
+      <Wrapper>
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Log in with Google"
+          onSuccess={handleLogin}
+          onFailure={handleLogin}
+          cookiePolicy="single_host_origin"
+        />
+      </Wrapper>
     </>
   );
 };
