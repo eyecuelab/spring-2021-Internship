41b004ad1c94d63e13a15b5ba943566db538aeb2
Zahnen Garner <zahnen@gmail.com>
2021-05-06 11:49:26 -0700

    add initial cost section styling

--- src/components/finance/index.tsx
+++ src/components/finance/index.tsx
@@ -1,36 +1,39 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { RiArrowDownSLine, RiArrowRightSLine } from 'react-icons/ri';
-import { FaPlus } from 'react-icons/fa';
 import { Table } from 'semantic-ui-react';
 
-const Wrapper = styled.div<{ open: boolean }>`
-  margin: 10px;
-  max-height: ${(props) => (props.open ? '100%' : '0')};
-  overflow: hidden;
-  padding: ${(props) => (props.open ? '25px 0' : '0')};
-  transition: all 0.3s ease-out;
+const Wrapper = styled.div`
+  position: relative;
+  margin-top: -50px;
+  padding: 25px 0;
 `;
 
-const HeadingContainer = styled.div`
-  background: #d1cfcf;
-  display: flex;
-  align-items: center;
-
-  justify-content: space-between;
-  width: 100%;
-`;
-const HeadingName = styled.h2`
-  margin: 0 10px 0 10px;
-  padding: 3px;
-  display: flex;
+const CategoryContainer = styled.div`
+  position: relative;
+  width: 1024px;
+  height: auto;
+  left: 48px;
+  background: ${(props) => props.theme.colors.white};
+  border-radius: 3px;
 `;
 
-const HeadingTotal = styled.h4<{ open: boolean }>`
-  opacity: ${(props) => (props.open ? 0 : 1)};
-  display: flex;
-  margin: 0px 10px;
-`;
+// const HeadingContainer = styled.div`
+//   background: #d1cfcf;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   width: 100%;
+// `;
+// const HeadingName = styled.h2`
+//   margin: 0 10px 0 10px;
+//   padding: 3px;
+// `;
+
+// const HeadingTotal = styled.h4`
+//   opacity: 0;
+//   display: flex;
+//   margin: 0px 10px;
+// `;
 
 type FinanceProps = {
   columnOne: string;
@@ -51,10 +54,6 @@ const Finance = ({
   handleToggleFinance,
   setDefaultForm,
 }: FinanceProps): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
   const addItem = (category: string): void => {
     handleToggleFinance();
     setDefaultForm(category);
@@ -62,45 +61,44 @@ const Finance = ({
 
   return (
     <>
-      <HeadingContainer>
-        <HeadingName onClick={handleClick}>
-          {isOpen ? <RiArrowDownSLine /> : <RiArrowRightSLine />}
-          {columnOne}
-        </HeadingName>
-        <HeadingTotal open={isOpen}>
+      {/* <HeadingContainer>
+        <HeadingName>{columnOne}</HeadingName>
+        <HeadingTotal>
           {columnOne === 'Activity'
             ? `Total: ${totals.toFixed(2)} hrs`
             : `Total: $${totals.toFixed(2)}`}
         </HeadingTotal>
-      </HeadingContainer>
-      <Wrapper open={isOpen}>
-        <Table basic="very" celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>{columnOne}</Table.HeaderCell>
-              <Table.HeaderCell>{columnTwo}</Table.HeaderCell>
-              <Table.HeaderCell>{columnThree}</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          {children}
-          <Table.Footer>
-            <Table.Row>
-              <Table.HeaderCell>
-                {columnOne === 'Material' ? <FaPlus onClick={() => addItem('materials')} /> : null}
+      </HeadingContainer> */}
+      <Wrapper>
+        <CategoryContainer>
+          <Table basic="very" celled style={{ marginTop: '50px', padding: '50px' }}>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>{columnOne}</Table.HeaderCell>
+                <Table.HeaderCell>{columnTwo}</Table.HeaderCell>
+                <Table.HeaderCell>{columnThree}</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            {children}
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell>
+                  {/* {columnOne === 'Material' ? <FaPlus onClick={() => addItem('materials')} /> : null}
                 {columnOne === 'Activity' ? <FaPlus onClick={() => addItem('labor')} /> : null}
-                {columnOne === 'Other Cost' ? <FaPlus onClick={() => addItem('other')} /> : null}
-              </Table.HeaderCell>
-              <Table.HeaderCell />
-              <Table.HeaderCell>
-                {columnOne === 'Activity' ? (
-                  <h2>Total Hours {totals.toFixed(2)}</h2>
-                ) : (
-                  <h2>Total ${totals.toFixed(2)}</h2>
-                )}
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
+                {columnOne === 'Other Cost' ? <FaPlus onClick={() => addItem('other')} /> : null} */}
+                </Table.HeaderCell>
+                <Table.HeaderCell />
+                <Table.HeaderCell>
+                  {columnOne === 'Activity' ? (
+                    <h2>Total Hours {totals.toFixed(2)}</h2>
+                  ) : (
+                    <h2>Total ${totals.toFixed(2)}</h2>
+                  )}
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </CategoryContainer>
       </Wrapper>
     </>
   );
--- src/components/projFinance/index.tsx
+++ src/components/projFinance/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import Finance from '../finance';
+import Tear from '../../assets/img/ProjectTear.svg';
 
 type ProjFinanceProps = {
   materialTotals: number;
@@ -11,6 +13,53 @@ type ProjFinanceProps = {
   handleToggleFinance: () => void;
   setDefaultForm: (taskStatus: string) => void;
 };
+
+const Wrapper = styled.div`
+  margin-top: 0px;
+  position: relative;
+  width: 1120px;
+  background: #fcfbf8;
+`;
+
+const Footer = styled.div`
+  position: relative;
+  margin-top: 0px;
+  z-index: 2;
+`;
+const Container = styled.div`
+  position: absolute;
+  height: 54px;
+  top: 73px;
+  border-radius: 3px;
+  background: ${(props) => props.theme.colors.white};
+`;
+
+const HeaderText = styled.p`
+  position: absolute;
+  top: 37px;
+  font-family: ${(props) => props.theme.font};
+  color: ${(props) => props.theme.colors.black};
+  opacity: 0.2;
+  line-height: 17px;
+  left: 48px;
+  font-size: 24px;
+`;
+
+const DetailText = styled.p`
+  color: ${(props) => props.theme.colors.navy};
+  position: relative;
+  font-family: Montserrat;
+  line-height: 17px;
+  margin-top: 17px;
+  top: 17px;
+`;
+
+const TrashIcon = styled.img`
+  position: relative;
+  margin-top: 17px;
+  margin-left: 22px;
+  cursor: pointer;
+`;
 
 const ProjFinance = ({
   materialTotals,
@@ -24,36 +73,43 @@ const ProjFinance = ({
 }: ProjFinanceProps): JSX.Element => {
   return (
     <>
-      <Finance
-        columnOne="Material"
-        columnTwo="Quantity"
-        columnThree="Cost (Per Unit)"
-        totals={materialTotals}
-        handleToggleFinance={handleToggleFinance}
-        setDefaultForm={setDefaultForm}
-      >
-        <>{materialItems}</>
-      </Finance>
-      <Finance
-        columnOne="Activity"
-        columnTwo="Hours"
-        columnThree="Date"
-        totals={laborTotals}
-        handleToggleFinance={handleToggleFinance}
-        setDefaultForm={setDefaultForm}
-      >
-        <>{laborItems}</>
-      </Finance>
-      <Finance
-        columnOne="Other Cost"
-        columnTwo=""
-        columnThree="Cost"
-        totals={otherTotals}
-        handleToggleFinance={handleToggleFinance}
-        setDefaultForm={setDefaultForm}
-      >
-        <>{otherItems}</>
-      </Finance>{' '}
+      <Wrapper>
+        <HeaderText>Cost</HeaderText>
+        <Finance
+          columnOne="Material"
+          columnTwo="Quantity"
+          columnThree="Cost (Per Unit)"
+          totals={materialTotals}
+          handleToggleFinance={handleToggleFinance}
+          setDefaultForm={setDefaultForm}
+        >
+          <>{materialItems}</>
+        </Finance>
+
+        <Finance
+          columnOne="Activity"
+          columnTwo="Hours"
+          columnThree="Date"
+          totals={laborTotals}
+          handleToggleFinance={handleToggleFinance}
+          setDefaultForm={setDefaultForm}
+        >
+          <>{laborItems}</>
+        </Finance>
+        <Finance
+          columnOne="Other Cost"
+          columnTwo=""
+          columnThree="Cost"
+          totals={otherTotals}
+          handleToggleFinance={handleToggleFinance}
+          setDefaultForm={setDefaultForm}
+        >
+          <>{otherItems}</>
+        </Finance>
+      </Wrapper>
+      <Footer>
+        <img src={Tear} alt="torn paper edge" />
+      </Footer>
     </>
   );
 };
