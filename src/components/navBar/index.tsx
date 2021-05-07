540cbeb10ae9850cdaac83805ee1903c01be9002
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-05-07 12:34:56 -0700

    add conditional render of navBar based on log in

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -23,6 +23,8 @@ import * as selectors from '../../store/selectors';
 
 const NavBar = (): JSX.Element => {
   const projectList = useSelector(selectors.selectProjectList);
+  const userUuid = useSelector(selectors.selectUUID);
+
   const [isOpenDropdown, setisOpenDropdown] = useState(false);
   const [isOpenNewProj, setisOpenNewProj] = useState(false);
   const dispatch = useDispatch();
@@ -62,79 +64,99 @@ const NavBar = (): JSX.Element => {
     if (projectList.length === 0) setisOpenDropdown(true);
   }, [projectList]);
 
-  return (
-    <>
-      <NavTwo>
-        <NavContainer>
-          <NavMenu>
-            <NavItem>
-              <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
-                Project
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
-                Tasks
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
-                Cost
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
-                Analysis
-              </NavLink>
-            </NavItem>
-          </NavMenu>
-        </NavContainer>
-      </NavTwo>
-      <Dropdown isOpen={isOpenDropdown} handleToggleDropdown={handleToggleDropdown} />
-      <Nav>
-        <NavContainer>
-          <NavLogo />
-          <NavMenu>
-            <NavItem>
-              {/* <NavLink color={theme.colors.white} size={theme.fontSizes.medium} to="/hub">
+  if (userUuid !== '') {
+    return (
+      <>
+        <NavTwo>
+          <NavContainer>
+            <NavMenu>
+              <NavItem>
+                <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
+                  Project
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
+                  Tasks
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
+                  Cost
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink color={theme.colors.teal} size={theme.fontSizes.small} to="/">
+                  Analysis
+                </NavLink>
+              </NavItem>
+            </NavMenu>
+          </NavContainer>
+        </NavTwo>
+        <Dropdown isOpen={isOpenDropdown} handleToggleDropdown={handleToggleDropdown} />
+        <Nav>
+          <NavContainer>
+            <NavLogo />
+            <NavMenu>
+              <NavItem>
+                {/* <NavLink color={theme.colors.white} size={theme.fontSizes.medium} to="/hub">
                 Hub
               </NavLink> */}
-            </NavItem>
-            <NavItem>
-              <DropDownLi
-                color={theme.colors.white}
-                size={theme.fontSizes.medium}
-                isOpen={isOpenDropdown}
-                onClick={handleClick}
-              >
-                Project
-                <RiArrowDownSLine />
-              </DropDownLi>
-            </NavItem>
-            <NavItem>
-              <AuthButton
-                onClick={userSignout}
-                color={theme.colors.white}
-                size={theme.fontSizes.medium}
-              >
-                Log out
-              </AuthButton>
-            </NavItem>
-          </NavMenu>
-        </NavContainer>
-      </Nav>
-      <Button
-        buttonText="New Project"
-        size={theme.fontSizes.medium}
-        margin="43px 47.73px auto 892px"
-        img={LgButton}
-        color={theme.colors.white}
-        handleToggle={handleToggleNewProj}
-      />
-      {isOpenNewProj && (
-        <NewProjectModal toggleModal={handleToggleNewProj} addProject={handleNewProject} />
-      )}
-    </>
+              </NavItem>
+              <NavItem>
+                <DropDownLi
+                  color={theme.colors.white}
+                  size={theme.fontSizes.medium}
+                  isOpen={isOpenDropdown}
+                  onClick={handleClick}
+                >
+                  Project
+                  <RiArrowDownSLine />
+                </DropDownLi>
+              </NavItem>
+              <NavItem>
+                <AuthButton
+                  onClick={userSignout}
+                  color={theme.colors.white}
+                  size={theme.fontSizes.medium}
+                >
+                  Log out
+                </AuthButton>
+              </NavItem>
+            </NavMenu>
+          </NavContainer>
+        </Nav>
+        <Button
+          buttonText="New Project"
+          size={theme.fontSizes.medium}
+          margin="43px 47.73px auto 892px"
+          img={LgButton}
+          color={theme.colors.white}
+          handleToggle={handleToggleNewProj}
+        />
+        {isOpenNewProj && (
+          <NewProjectModal toggleModal={handleToggleNewProj} addProject={handleNewProject} />
+        )}
+      </>
+    );
+  }
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo />
+        {/* <NavMenu>
+          <NavItem>
+            <AuthButton
+              onClick={userSignout}
+              color={theme.colors.white}
+              size={theme.fontSizes.medium}
+            >
+              Log out
+            </AuthButton>
+          </NavItem>
+        </NavMenu> */}
+      </NavContainer>
+    </Nav>
   );
 };
 
