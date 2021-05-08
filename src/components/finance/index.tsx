182653911272dd9fce229e494db5cb1bf499417c
Zahnen Garner <zahnen@gmail.com>
2021-05-07 17:01:18 -0700

    add initial updated "Add Item" functionality

--- src/components/finance/index.tsx
+++ src/components/finance/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import SmallButton from '../smallButton';
 import SmButton from '../../assets/img/SmButton.svg';
 import theme from '../../styles/theme';
+import Item from '../item';
 
 const Wrapper = styled.div`
   position: relative;
@@ -75,19 +76,33 @@ const Finance = ({
     handleToggleFinance();
     setDefaultForm(category);
   };
+  const [newItem, setNewItem] = useState(false);
 
   return (
     <>
       <Wrapper>
         <Header>{columnOne}</Header>
-        <CategoryContainer>{children}</CategoryContainer>
+        <CategoryContainer>
+          {children}
+          {newItem ? (
+            <Item
+              id="test"
+              itemName="Enter Name"
+              itemPrice={0}
+              quantity={0}
+              handleDelete={handleToggleFinance}
+              handleUpdateItem={handleToggleFinance}
+              category="material"
+            />
+          ) : null}
+        </CategoryContainer>
         <SmallButton
           buttonText={columnTwo}
           size="12px"
           margin="10px 10px 10px 65px"
           img={SmButton}
           color={theme.colors.white}
-          onClick={() => addItem(columnThree)}
+          onClick={() => setNewItem(!newItem)}
         />
         <TotalsWrapper>
           <TotalsText>
