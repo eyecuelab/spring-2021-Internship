fa7850e592d874f7ddd332dcf6936893abef542b
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-05-10 14:42:15 -0700

    remove number input spinners

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,6 +6,17 @@ body {
   margin: auto;
   overflow-x: hidden;
 }
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
+
+input[type=number] {
+    -moz-appearance:textfield; /* Firefox */
+}
 `;
 
 export default GlobalStyles;
